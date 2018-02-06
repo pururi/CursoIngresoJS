3d9c9af5cbfa2e6acceb2a88237ccc5493830abb
Pablo del Rio <35604158+pururi@users.noreply.github.com>
2018-02-06 10:17:09 -0300

    Ejercicio 1 y 2
    Variable, Alert, Concatenar Prompt etc

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -2,6 +2,29 @@
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
 function Mostar()
 {
+	var nombre;
+
+	nombre=prompt("ingrese nombre","Pablo");
+
+	alert("Su nombre es: "+nombre);
+
+	//esto es un comentario de una linea
+
+	//alert (nombre);
+
+	/*
+
+	esto es un bloque
+
+	alert ("nombre");
+
+	nombre="Pablo";
+
+	alert(nombre);
 	
+	*/
+
+	//var person = prompt("Please enter your name", "Harry Potter");
+
 }
 
